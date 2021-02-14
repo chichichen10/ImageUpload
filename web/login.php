4f47bea15969be3742569146053ddb2d10503f8d
CHEN Ying Chi <chichichen10@gmail.com>
2021-02-15 04:24:57 +0800

    login page without sql

--- web/login.php
+++ web/login.php
@@ -1,14 +1,23 @@
 <?php
+if(!isset($_SESSION)){
+    session_start();
+    }
 
 $id=$_POST['id'];
 $pwd=$_POST['pwd'];
-if($id=="user"&&$pwd=="123"){
-    echo "Logged in.";
-    echo '<meta http-equiv=REFRESH CONTENT=1;url=hi.php>';
-}else{
-    echo "Failed.";
-    echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
+if($_SESSION['check_word']==$_POST['check']){
+    $_SESSION['check_word'] = ''; 
+    if($id=="user"&&$pwd=="123"){
+        echo "Logged in.";
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=hi.php>';
+    }else{
+        echo "Wrong username or password.";
+        echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
 
+    }
+}else{
+    echo "Wrong Verification!";
+    echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
 }
 
 ?>
