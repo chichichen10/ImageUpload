a4eab64b127de3f1a8245b5c661c655c07d102cc
CHEN Ying Chi <chichichen10@gmail.com>
2021-02-15 14:17:52 +0800

    new index

--- web/index.php
+++ web/index.php
@@ -1,11 +1,4 @@
 <!-- 設定網頁編碼為UTF-8 -->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<form name="form" method="post" action="login.php">
-Username：<input type="text" name="id" /> <br>
-Password：<input type="password" name="pwd" /> <br>
-Input the folllowing numbers:<br>
-<img id="imgcode" src="captcha.php" /><br />
-<input type="text" name="check"><br>
-<input type="submit" name="button" value="Login" />&nbsp;&nbsp;
-<a href="captcha_index.php">captcha</a>
+<h1>This is Index</h1>
+<a href="loginpage.php">Click here to Login</a>
 </form>
---
+++ web/loginpage.php
@@ -0,0 +1,10 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="form" method="post" action="login.php">
+    Username：<input type="text" name="id" /> <br>
+    Password：<input type="password" name="pwd" /> <br>
+    Input the folllowing numbers:<br>
+    <img id="imgcode" src="captcha.php" /><br />
+    <input type="text" name="check"><br>
+    <input type="submit" name="button" value="Login" />&nbsp;&nbsp;
+    <a href="captcha_index.php">captcha</a>
+</form>
