21c9caedd0fe6b829f9a4b92663a085b759c6c20
CHEN Ying Chi <chichichen10@gmail.com>
2021-02-15 19:33:32 +0800

    fix

--- web/index.php
+++ web/index.php
@@ -3,10 +3,10 @@
 <?php
 if ($_SESSION['username'] != null) {
     $username = $_SESSION['username'];
-    echo '<h3>WelCome <l> $username <l>!</h3>';
+    echo '<h3>WelCome <l> {$username} </l>!</h3>';
     echo '<a href="logout.php">Logout</a>';
 } else {
-    echo '<h3> Hi guest</h3>';
+    echo '<h3> Hi guest!</h3>';
     echo '<a href="loginpage.php">Click here to Login</a>';
 }
 ?>
