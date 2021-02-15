3ce68efee146aa4d0df1bcdf0e39c175a85e7eeb
CHEN Ying Chi <chichichen10@gmail.com>
2021-02-15 13:15:56 +0800

    connect to database

--- web/login.php
+++ web/login.php
@@ -1,23 +1,36 @@
 <?php
-if(!isset($_SESSION)){
+if (!isset($_SESSION)) {
     session_start();
-    }
+}
+$db = parse_url(getenv("DATABASE_URL"));
 
-$id=$_POST['id'];
-$pwd=$_POST['pwd'];
-if($_SESSION['check_word']==$_POST['check']){
-    $_SESSION['check_word'] = ''; 
-    if($id=="user"&&$pwd=="123"){
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+
+$id = $_POST['id'];
+$pwd = $_POST['pwd'];
+
+$sql = "SELECT * FROM myusers WHERE name = '$id'";
+$result = $pdo->query($sql);
+$row = $result->fetchAll();
+
+if ($_SESSION['check_word'] == $_POST['check']) {
+    $_SESSION['check_word'] = '';
+    if ($id != null && $pwd != null && $id == $row[1] && $pwd == $row[2]) {
         echo "Logged in.";
         echo '<meta http-equiv=REFRESH CONTENT=1;url=hi.php>';
-    }else{
+    } else {
         echo "Wrong username or password.";
         echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
-
     }
-}else{
+} else {
     echo "Wrong Verification!";
     echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
 }
-
-?>
