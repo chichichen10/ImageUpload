dc631d59ea27fb5573c0887bdac4b8fed25b7bb1
CHEN Ying Chi <chichichen10@gmail.com>
2021-02-15 22:24:26 +0800

    fix

---
+++ web/view.php
@@ -0,0 +1,18 @@
+<?php
+
+$sql = "SELECT * FROM images";
+
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+$result = $pdo->query($sql);
+$raw = $result->fetch();
+echo $result['image'];
