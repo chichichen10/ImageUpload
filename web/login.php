d12467e2610e279a7f288bff252897f428bad326
CHEN Ying Chi <chichichen10@gmail.com>
2021-02-18 21:16:04 +0800

    fix

--- web/login.php
+++ web/login.php
@@ -26,7 +26,7 @@ if ($_SESSION['check_word'] == $_POST['check']) {
     if ($id != null && $pwd != null && $id == $row[1] && $pwd == $row[2]) {
         $_SESSION['username'] = $id;
         echo "Logged in.";
-        if ($id == 'Admin')
+        if ($id == 'admin')
             echo '<meta http-equiv=REFRESH CONTENT=1;url=initializepage.php>';
         else
             echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
